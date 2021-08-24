5c786e39e2b2312a8caff4096b5368b735ea5c5d
Robson Rosa <contatorobsonrosa@gmail.com>
2021-08-24 00:27:22 -0300

    consulta API Via CEP

--- script/script.js
+++ script/script.js
@@ -1,21 +1,3 @@
-import axios from 'axios';
-
-function validarCEP() {
-    let cep = document.getElementById('cep').value;
-    console.log(`CEP: ${cep}`)
-
-    document.getElementById('erroCEP').style.display = 'none';
-    document.getElementById('erroBlocoCEP').style.border = 'none';
-
-    if (!cep) {
-        document.getElementById('erroCEP').style.display = 'block';
-        document.getElementById('erroBlocoCEP').style.border = '.1875rem solid red';
-        return false;
-    } else {
-        return true;
-    }
-}
-
 function validaCPF(cpf) {
     console.log(cpf.length);
     if (cpf.length != 11) {
@@ -70,67 +52,99 @@ function validacaoCPF() {
     }
 }
 
-function validacaoEmail() {
-    let email = document.forms['container']['email'].value;
-    if (!email) {
-        document.getElementById('erroEmail').style.display = 'block';
-        document.getElementById('erroBlocoEmail').style.border = '.1875rem solid red';
-        return false;
+document.getElementById('cpf').addEventListener('focusout', validacaoCPF);
+
+// function validacaoEmail() {
+//     let email = document.forms['container']['email'].value;
+//     if (!email) {
+//         document.getElementById('erroEmail').style.display = 'block';
+//         document.getElementById('erroBlocoEmail').style.border = '.1875rem solid red';
+//         return false;
+//     } else {
+//         document.getElementById('erroEmail').style.display = 'none';
+//         document.getElementById('erroBlocoEmail').style.border = 'none';
+//         return true;
+//     }
+// }
+
+// function validacaoIdentidade() {
+//     let id = document.forms['container']['identidade'].value;
+//     if (!id) {
+//         document.getElementById('erroId').style.display = 'block';
+//         document.getElementById('erroBlocoId').style.border = '.1875rem solid red';
+//         return false;
+//     } else {
+//         document.getElementById('erroId').style.display = 'none';
+//         document.getElementById('erroBlocoId').style.border = 'none';
+//         return true;
+//     }
+// }
+
+// function validacaoNascimento() {
+
+//     let dia = document.getElementById('dia');
+//     let mes = document.getElementById('mes');
+//     let ano = document.getElementById('ano');
+//     let aux = dia.options[dia.selectedIndex].text;
+//     let aux2 = mes.options[mes.selectedIndex].text;
+//     let aux3 = ano.options[ano.selectedIndex].text;
+//     if (!aux || !aux2 || !aux3) {
+//         document.getElementById('erroNascimento').style.display = 'block';
+//         document.getElementById('dataNascimento').style.border = '.1875rem solid red';
+//         return false;
+//     } else {
+//         document.getElementById('erroNascimento').style.display = 'none';
+//         document.getElementById('dataNascimento').style.border = 'none';
+//         return true;
+//     }
+// }
+
+const validaCEP = (cep) => cep.toString().length == 8;
+
+const buscaCEP = async () => {
+    LimpaEndereco();
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (validaCEP(cep)) {
+        const data = await fetch(url);
+        const endereco = await data.json();
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('erroCEP').style.display = 'block';
+            document.getElementById('erroBlocoCEP').style.border = '.1875rem solid red';
+        } else {
+            document.getElementById('erroCEP').style.display = 'none';
+            document.getElementById('erroBlocoCEP').style.border = 'none';
+            completaEndereco(endereco);
+        }
     } else {
-        document.getElementById('erroEmail').style.display = 'none';
-        document.getElementById('erroBlocoEmail').style.border = 'none';
-        return true;
+        document.getElementById('erroCEP').style.display = 'block';
+        document.getElementById('erroBlocoCEP').style.border = '.1875rem solid red';
     }
 }
 
-function validacaoIdentidade() {
-    let id = document.forms['container']['identidade'].value;
-    if (!id) {
-        document.getElementById('erroId').style.display = 'block';
-        document.getElementById('erroBlocoId').style.border = '.1875rem solid red';
-        return false;
-    } else {
-        document.getElementById('erroId').style.display = 'none';
-        document.getElementById('erroBlocoId').style.border = 'none';
-        return true;
-    }
+document.getElementById('cep').addEventListener('focusout', buscaCEP);
+
+const completaEndereco = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
 }
 
-function validacaoNascimento() {
-
-    let dia = document.getElementById('dia');
-    let mes = document.getElementById('mes');
-    let ano = document.getElementById('ano');
-    let aux = dia.options[dia.selectedIndex].text;
-    let aux2 = mes.options[mes.selectedIndex].text;
-    let aux3 = ano.options[ano.selectedIndex].text;
-    if (!aux || !aux2 || !aux3) {
-        document.getElementById('erroNascimento').style.display = 'block';
-        document.getElementById('dataNascimento').style.border = '.1875rem solid red';
-        return false;
-    } else {
-        document.getElementById('erroNascimento').style.display = 'none';
-        document.getElementById('dataNascimento').style.border = 'none';
-        return true;
-    }
+const LimpaEndereco = () => {
+    document.getElementById('endereco').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";
 }
 
 function check_form() {
     var valid = true;
 
-    if (!validacaoCPF() || !validarCEP()) { valid = false; }
+    if (!validacaoCPF() || !buscaCEP()) { valid = false; }
 
     if (!valid) {
-        alert('Por favor preencha todos os campos corretamente.');
+        alert('Por favor, preencha todos os campos corretamente.');
         return false;
     } else { return true; }
 }
-
-function validacoes() {
-    validacaoCPF();
-    validarCEP();
-    validacaoEmail();
-    validacaoNascimento();
-    validacaoIdentidade();
-}
-
