a7deb70cca33f4ba04679a09ed16f8ab09048bdb
Robson Rosa <contatorobsonrosa@gmail.com>
2021-08-28 09:54:06 -0300

    integração do front com o back

--- script/script.js
+++ script/script.js
@@ -1,103 +1,56 @@
-function validaCPF(cpf) {
-    console.log(cpf.length);
-    if (cpf.length != 11) {
-        return false;
-    } else {
-
-        var numeros = cpf.substring(0, 9);
-        var digitos = cpf.substring(9);
-        var soma = 0
-        for (var i = 10; i > 1; i--) {
-            soma += numeros.charAt(10 - i) * i
-        }
-
-        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-        //validação do primeiro digito
-        if (resultado != digitos.charAt(0)) {
-            return false;
-        }
-
-        soma = 0;
-        numeros = cpf.substring(0, 10);
-
-        for (var k = 11; k > 1; k--) {
-            soma += numeros.charAt(11 - k) * k;
-        }
-
-        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-        //validação segundo digito
-        if (resultado != digitos.charAt(1)) {
-            return false;
-        }
-
-        return true;
-    }
-}
-
-function validacaoCPF() {
-    let cpf = document.getElementById('cpf').value;
-
-    let resultadoValidacao = validaCPF(cpf);
-
-    if (!resultadoValidacao) {
-        document.getElementById('erroCPF').style.display = 'block';
-        document.getElementById('erroBlocoCPF').style.border = '.1875rem solid red';
-        return false;
-    } else {
-        document.getElementById('erroCPF').style.display = 'none';
-        document.getElementById('erroBlocoCPF').style.border = 'none';
-        return true;
-    }
-}
-
-document.getElementById('cpf').addEventListener('focusout', validacaoCPF);
-
-// function validacaoEmail() {
-//     let email = document.forms['container']['email'].value;
-//     if (!email) {
-//         document.getElementById('erroEmail').style.display = 'block';
-//         document.getElementById('erroBlocoEmail').style.border = '.1875rem solid red';
+// function validaCPF(cpf) {
+//     if (cpf.length != 11) {
 //         return false;
 //     } else {
-//         document.getElementById('erroEmail').style.display = 'none';
-//         document.getElementById('erroBlocoEmail').style.border = 'none';
+//         var numeros = cpf.substring(0, 9);
+//         var digitos = cpf.substring(9);
+//         var soma = 0
+//         for (var i = 10; i > 1; i--) {
+//             soma += numeros.charAt(10 - i) * i
+//         }
+
+//         var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+//         //validação do primeiro digito
+//         if (resultado != digitos.charAt(0)) {
+//             return false;
+//         }
+
+//         soma = 0;
+//         numeros = cpf.substring(0, 10);
+
+//         for (var k = 11; k > 1; k--) {
+//             soma += numeros.charAt(11 - k) * k;
+//         }
+
+//         resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+//         //validação segundo digito
+//         if (resultado != digitos.charAt(1)) {
+//             return false;
+//         }
+
 //         return true;
 //     }
 // }
 
-// function validacaoIdentidade() {
-//     let id = document.forms['container']['identidade'].value;
-//     if (!id) {
-//         document.getElementById('erroId').style.display = 'block';
-//         document.getElementById('erroBlocoId').style.border = '.1875rem solid red';
+// function validacaoCPF() {
+//     let cpf = document.getElementById('cpf').value;
+
+//     let resultadoValidacao = validaCPF(cpf);
+
+//     if (!resultadoValidacao) {
+//         document.getElementById('erroCPF').style.display = 'block';
+//         document.getElementById('erroBlocoCPF').style.border = '.1875rem solid red';
 //         return false;
 //     } else {
-//         document.getElementById('erroId').style.display = 'none';
-//         document.getElementById('erroBlocoId').style.border = 'none';
+//         document.getElementById('erroCPF').style.display = 'none';
+//         document.getElementById('erroBlocoCPF').style.border = 'none';
 //         return true;
 //     }
 // }
 
-// function validacaoNascimento() {
-
-//     let dia = document.getElementById('dia');
-//     let mes = document.getElementById('mes');
-//     let ano = document.getElementById('ano');
-//     let aux = dia.options[dia.selectedIndex].text;
-//     let aux2 = mes.options[mes.selectedIndex].text;
-//     let aux3 = ano.options[ano.selectedIndex].text;
-//     if (!aux || !aux2 || !aux3) {
-//         document.getElementById('erroNascimento').style.display = 'block';
-//         document.getElementById('dataNascimento').style.border = '.1875rem solid red';
-//         return false;
-//     } else {
-//         document.getElementById('erroNascimento').style.display = 'none';
-//         document.getElementById('dataNascimento').style.border = 'none';
-//         return true;
-//     }
-// }
+// document.getElementById('cpf').addEventListener('focusout', validacaoCPF);
 
 const validaCEP = (cep) => cep.toString().length == 8;
 
@@ -143,13 +96,60 @@ const LimpaEndereco = () => {
     document.getElementById('estado').value = "";
 }
 
-function check_form() {
-    var valid = true;
+const Formulario = () => {
+    let form = {
+        nome: document.getElementById('nome').value,
+        cargo: document.getElementById('cargo').value,
+        dia: document.getElementById('dia').value,
+        mes: document.getElementById('mes').value,
+        ano: document.getElementById('ano').value,
+        estadoCivil: document.getElementById('estadoCivil').value,
+        genero: document.getElementById('genero').value,
+        cep: document.getElementById('cep').value,
+        endereco: document.getElementById('endereco').value,
+        numResidencia: document.getElementById('numResidencia').value,
+        bairro: document.getElementById('bairro').value,
+        cidade: document.getElementById('cidade').value,
+        estado: document.getElementById('estado').value,
+        telefone: document.getElementById('telefone').value,
+        celular: document.getElementById('celular').value,
+        email: document.getElementById('email').value,
+        identidade: document.getElementById('identidade').value,
+        cpf: document.getElementById('cpf').value,
+        possuiveiculo: document.getElementById('possuiveiculo').value,
+        categoriasHabilitacao: document.getElementById('categoriasHabilitacao').value,
+    };
+    console.log(form);
+    return form
+}
 
+const criarCandidato = async (candidato) => {
+    try {
+        const usuario = fetch('http://localhost:5000/registro', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Formulario())
+        });
+        if (usuario.status === 200) {
+            alert('DEU CERTO')
+        }
+    } catch (error) {
+        alert('deu errado!');
+    }
+}
+
+function check_form() {
+
+    var valid = true;
     if (!validacaoCPF() || !buscaCEP()) { valid = false; }
 
     if (!valid) {
         alert('Por favor, preencha todos os campos corretamente.');
         return false;
-    } else { return true; }
+    } else {
+        return true;
+    }
 }
